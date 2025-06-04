26eb01342da9a60d001cf7cbe59b2cafd1467b8b
dhullubasnkot <123baglung@gmail.com>
2025-06-04 16:07:41 +0545

    install mongoDB

---
+++ src/model/MongoDbModel/mongodb-client.ts
@@ -0,0 +1,15 @@
+import mongoose, { Mongoose } from "mongoose";
+
+const url = process.env.MONGO_URL;
+mongoose.connect(
+  "mongodb+srv://123baglung:balgita@89@cluster0.bouubfj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+);
+const db = mongoose.connection;
+
+db.on("error", (err) => {
+  console.log("Conection Refused", err);
+});
+db.once("open", function () {
+  console.log("Connected to MongoDB!");
+});
+export default mongoose;
---
+++ src/model/MongoDbModel/user/user.mongodb-Model.ts
@@ -0,0 +1,10 @@
+import { Schema } from "mongoose";
+import mongoose from "../mongodb-client";
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
